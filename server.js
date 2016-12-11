7500c4a9c6f0ddf851da26cd26eeb37bfedf23dd
Dennis Wolters <dbn@gmx.de>
2016-12-11 22:18:54 +0100

    Fixed linting errors

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ if(process.env.TRAVIS && process.argv.indexOf('detached') === -1) {
     const child = spawn('node', ['server.js', 'detached'], {detached: true, stdio: 'ignore'});
     child.unref();
     console.log('Spawning detached server');
-    setTimeout(() => {console.log('Leaving main process')}, 3000);
+    setTimeout(() => {
+		console.log('Leaving main process');
+	}, 3000);
 } else {
     const port = 3000;
     const app = require('./app.js');
